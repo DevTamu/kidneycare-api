9c2797b2e9fb98e2183a03f97b32917aaf53f143
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-03 23:44:35 +0800

    Setup Serializer in News Event API

---
+++ kidney/app_news_event/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+class AddAppointmentSerializer():
+    pass
+
+class GetPendingAppointmentSerializer():
+    pass
+
+class GetAppointmentsSerializer():
+    pass
+
+class GetAppointmentSerializer():
+    pass
+
+class UpdateAppointmentSerializer():
+    pass
--- kidney/app_news_event/views.py
+++ kidney/app_news_event/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .serializers import (
+    AddAppointmentSerializer,
+    GetPendingAppointmentSerializer,
+    GetAppointmentsSerializer,
+    GetAppointmentSerializer,
+    UpdateAppointmentSerializer
+)
 # Create your views here.
