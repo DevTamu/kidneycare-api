f5631cf78d6f47adf20588be5303d6538e46e7cc
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-07 12:07:59 +0800

    Changed Allowed HOSTS

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["anxious-misti-devtamu-3916140d.koyeb.app"]
 
 
 # Application definition
