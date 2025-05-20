d40afe04b8b741f9a1d67bde4f561bb434ad658c
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-20 16:13:45 +0800

    Removed the API from the url

--- kidney/kidney/urls.py
+++ kidney/kidney/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('app_authentication.urls')),
     path('news-event/', include('app_news_event.urls')),
     path('api/', include('app_appointment.urls')),
-    path('api/', include('app_treatment.urls'))
+    path('', include('app_treatment.urls'))
 ]
 
 
