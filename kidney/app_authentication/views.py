25881cb17f2e7f2ba3722e50e8b5ff8c379dd4df
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-24 14:36:36 +0800

    LAtest commit dev

--- kidney/app_authentication/views.py
+++ kidney/app_authentication/views.py
@@ -435,8 +435,10 @@ class EditProfileInPatientView(generics.UpdateAPIView):
                     "user_image": request.build_absolute_uri(user_profile.picture.url) if user_profile.picture else None
                 }, status_code=status.HTTP_200_OK)
                 return ResponseMessageUtils(message="Successfully updated your profile", status_code=status.HTTP_200_OK)
+            print(serializer.errors)
             return ResponseMessageUtils(message=extract_first_error_message(serializer.errors), status_code=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
+            print(f'qwewqe: {e}')
             return ResponseMessageUtils(
                 message="Something went wrong while processing your request.",
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
