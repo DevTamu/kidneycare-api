bcd2ceefd2809d7377173722e3efb8473f2048fe
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-24 16:10:18 +0800

    Added kidneycare-api in ALLOWED HOST

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -36,14 +36,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')    
 
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ["kidneycare-api.onrender.com"]
+ALLOWED_HOSTS = ['kidneycare-api.onrender.com', 'localhost', '127.0.0.1']
 
 
 
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://kidneycare-api.onrender.com',  # or your custom domain
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://kidneycare-api.onrender.com',  # or your custom domain
+# ]
 
 
 
