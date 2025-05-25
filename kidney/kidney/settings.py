36d49656f44bf2a8b34881ed4dae8479f35e562b
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-25 22:43:12 +0800

    Uncomment DEBUG

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -33,7 +33,7 @@ cloudinary.config(
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG')    
+DEBUG = os.environ.get('DEBUG')    
 
 ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = ["kidneycare-api.onrender.com"]
