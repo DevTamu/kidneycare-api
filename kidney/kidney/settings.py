f5707795d34f1e972cc2ed4bc406e945a8ac9cd1
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-25 23:39:12 +0800

    Removed unused code

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -8,11 +8,6 @@ from django.core.files.storage import default_storage
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
-    'API_KEY': os.environ.get('API_KEY'),
-    'API_SECRET': os.environ.get('API_SECRET'),
-}
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
