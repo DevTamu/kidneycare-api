6276d142ee92d9713534b989588ff6e6d6805638
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-26 16:11:25 +0800

    Changed access token lifetime

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1), #5 minute access tokens
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3), #5 minute access tokens
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1), # 1 day refresh tokens
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
