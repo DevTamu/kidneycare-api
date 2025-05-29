11026f55857b74c417fcacdfaa30710d369214c7
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-29 14:35:10 +0800

    Fixed chat

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -100,23 +100,6 @@ WSGI_APPLICATION = 'kidney.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DATABASE_NAME'),
-#         'USER': os.environ.get('DATABASE_USER'),
-#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-#         'HOST': os.environ.get('DATABASE_HOST'),
-#         'OPTIONS': {'sslmode': 'require'},
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -190,17 +173,6 @@ SIMPLE_JWT = {
 }
 
 ASGI_APPLICATION = 'kidney.asgi.application'
-
-
-
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
-#         },
-#     },
-# }
 
 CHANNEL_LAYERS = {
     "default": {
