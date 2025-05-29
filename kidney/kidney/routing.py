07db9731b15911560be9d0610d4a82a62311e383
mrtnbtstaa <martinbautista0288@outlook.com>
2025-05-29 23:37:07 +0800

    Still Fixing the websocket connection.

--- kidney/kidney/routing.py
+++ kidney/kidney/routing.py
@@ -1,21 +1,8 @@
 # routing.py
 from django.urls import re_path
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 from . import consumers
-from .middleware.token_auth_middleware import JWTAuthMiddleware
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
-    # re_path(r"ws/chat/(?P<room_name>[a-f0-9]{32})/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>[a-f0-9]{32})/$", consumers.ChatConsumer.as_asgi()),
 ]
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": JWTAuthMiddleware(
-        AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        )
-    ),
-})
