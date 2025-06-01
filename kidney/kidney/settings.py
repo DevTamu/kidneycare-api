f8b010156be5a6b6e07eabb8a58389e60b86b8f1
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-01 23:29:50 +0800

    latest

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -110,12 +110,7 @@ WSGI_APPLICATION = 'kidney.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # DATABASES = {
