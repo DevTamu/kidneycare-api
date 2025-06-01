6b39944d77d09e8c07c2769a46f681ce0deac52c
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-01 23:32:19 +0800

    Changed the settings config

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -29,17 +29,17 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')    
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
 
-# ALLOWED_HOSTS = ["kidneycare-api.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["kidneycare-api.onrender.com", "localhost", "127.0.0.1"]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://kidneycare-api.onrender.com',
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://kidneycare-api.onrender.com',
+]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'https://kidneycare-api.onrender.com',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://kidneycare-api.onrender.com',
+]
 
 redis_url = os.environ.get('REDIS_URL')
 
@@ -110,21 +110,21 @@ WSGI_APPLICATION = 'kidney.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         ssl_require=True,
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
 
 
 # Password validation
