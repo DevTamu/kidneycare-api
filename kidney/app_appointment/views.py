0182e74ad9b4f131acc7a8c173852a80a67f9730
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-03 14:39:45 +0800

    added appointment_pagination.py

--- kidney/app_appointment/views.py
+++ kidney/app_appointment/views.py
@@ -26,13 +26,7 @@ from kidney.utils import (
 from datetime import timedelta
 from rest_framework.permissions import IsAuthenticated
 from .models import AssignedAppointment
-from rest_framework.pagination import PageNumberPagination
-
-
-class AppointmentPagination(PageNumberPagination):
-    page_size = 20  #define how many appointments to show per page
-    page_size_query_param = 'page'  # Allow custom page size via query params
-    max_page_size = 25  # Maximum allowed page size
+from kidney.pagination.appointment_pagination import AppointmentPagination
 
 class CreateAppointmentView(generics.CreateAPIView):
 
