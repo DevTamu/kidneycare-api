8fb2b528beb31617dd6bc872b9a2c6d8c046918c
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-04 13:12:03 +0800

    fixed the response

--- kidney/app_chat/urls.py
+++ kidney/app_chat/urls.py
@@ -3,6 +3,7 @@ from .views import (
     GetNotificationChatsToProviderView,
     UpdateNotificationChatInProviderView,
     GetProvidersChatView,
+    # UpdateChatStatusInPatientView,
     GetProviderChatInformationView,
     GetPatientsChatView,
     GetPatientChatInformationView
@@ -11,8 +12,9 @@ from .views import (
 urlpatterns = [
     path("chat/notifications/", GetNotificationChatsToProviderView.as_view(), name='chat-notifications-list'),
     path("chat/<int:pk>/notifications/", UpdateNotificationChatInProviderView.as_view(), name='chat-notifications'),
-    path("providers/chats/", GetProvidersChatView.as_view(), name='providers-chat'),
+    path("providers/chat/", GetProvidersChatView.as_view(), name='providers-chat'),
     path("providers/<str:pk>/chat/messages/", GetProviderChatInformationView.as_view(), name='providers-pk-chat'),
     path("patients/chat/", GetPatientsChatView.as_view(), name='patients-chat'),
+    # path("patients/chat/<int:pk>/", UpdateChatStatusInPatientView.as_view(), name='update-chat-status'),
     path("patients/<str:pk>/chat/messages/", GetPatientChatInformationView.as_view(), name='patients-pk-chat'),
 ]
