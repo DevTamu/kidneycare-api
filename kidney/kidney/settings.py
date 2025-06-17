e729e46e698ba3ad4069e0cd67dc670d1dff7ce6
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-17 12:28:01 +0800

    chore:(changed the configuration setup in the settings)

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -5,10 +5,15 @@ from dotenv import load_dotenv
 import os
 from django.core.files.storage import default_storage
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
 import dj_database_url
 import urllib.parse
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_mode = os.environ.get('DJANGO_ENV', 'production')
+load_dotenv(os.path.join(BASE_DIR, f".env.{env_mode}"))
+
+print(f"RUNNING AT ENV: .env.{env_mode}")
+
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
@@ -27,11 +32,10 @@ cloudinary.config(
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')    
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'  
 
-# ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS = ["kidneycare-api.onrender.com", "www.kidneycare-api.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://kidneycare-api.onrender.com',
