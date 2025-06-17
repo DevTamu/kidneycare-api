d7ae97cc9e150cf7062b2bb546e90fd2e2638e33
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-17 13:42:52 +0800

    latest

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -32,10 +32,12 @@ cloudinary.config(
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True' 
+DEBUG = os.environ.get('DEBUG')
+# DEBUG = os.environ.get('DEBUG', 'False') == 'True' 
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://kidneycare-api-dev.onrender.com',
