b9307089ef1f3b6b883d732451a5c38bb7de3ce0
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-17 12:54:36 +0800

    chore:(changed the configuration setup in the settings)

--- kidney/kidney/settings.py
+++ kidney/kidney/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'  
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://kidneycare-api.onrender.com',
