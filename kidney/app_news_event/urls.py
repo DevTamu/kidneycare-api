c187c25ee0f228cc5e4c79be96fb8c00c676cbcb
mrtnbtstaa <martinbautista0288@outlook.com>
2025-06-22 10:51:48 +0800

    added news event for mobile API

--- kidney/app_news_event/urls.py
+++ kidney/app_news_event/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     AddNewsEventView,
     GetNewsEventView,
+    GetNewsEventsView,
     GetNewsEventLimitByTwoView,
     GetNewsEventWithIDView
 )
@@ -9,6 +10,7 @@ from .views import (
 urlpatterns = [
     path('add/news-event/', AddNewsEventView.as_view(), name='add'),
     path('news-event/', GetNewsEventView.as_view(), name='get-news-event'),
+    path('news-events/', GetNewsEventsView.as_view(), name='get-news-events'),
     path('news-event/<int:pk>/', GetNewsEventWithIDView.as_view(), name='get-news-event-by-id'),
     # path('get/limit-news-event/', GetNewsEventLimitByTwoView.as_view(), name='get-news-event-limit'),
 ]
